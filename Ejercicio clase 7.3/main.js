9808a150429bc839731f16dd7c76984ec929c4a4
Cami <cami.pletscher@gmail.com>
2022-03-08 20:57:51 -0300

    Ejercicio clase 7/3
    Crear un script completo, pidiendo minimo 1 dato y realizando operacion matematica o concatenacion

---
+++ Ejercicio clase 7.3/main.js
@@ -0,0 +1,17 @@
+let nombre = prompt("Ingrese su nombre");
+let apellido = prompt("Ingrese su apellido");
+
+alert("Hola " + nombre + " " + apellido);
+
+
+
+let anioNacimiento = parseInt(prompt("Ingrese año de nacimiento"));
+const anioActual = 2022; 
+let edad = anioActual - anioNacimiento; 
+
+alert("Usted tiene " + edad + " años");
+
+
+let cuadroFutbol = prompt("Ingrese su equipo de futbol favorito"); 
+
+alert("Usted es simpatizante de " + cuadroFutbol); 
