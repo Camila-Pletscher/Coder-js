9616816876c3df8fbf000f95263c40a71fcbb2e8
Cami <cami.pletscher@gmail.com>
2022-03-08 00:35:16 -0300

    Clase 7/3
    Sintaxis, variables. Prompt, alert, console.log. Comentarios

---
+++ Clase 7.3/main.js
@@ -0,0 +1,65 @@
+let apellido = "Pletscher"; 
+const curso = "javaScript";
+
+// Declarar variable 
+
+let numero; 
+
+// Asignando un valor a variable numero 
+
+numero = 10; 
+
+// Otra forma de declarar y asignar valor al mismo tiempo 
+
+let numero1 = 11;
+
+// TIPOS DE DATOS
+
+let numero2 = 30; // tipo number
+
+let nombre = "Camila"; // tipo string
+
+//TIPOS DE VALORES Y ASIGNACION 
+
+let variableNumerica = 10; 
+let variableTexto = "Esto es un texto";
+console.log(variableTexto);
+
+variableTexto = "Esto es otro texto";
+console.log(variableTexto);
+
+// Operacion basicas 
+
+let numeroA = 3;
+let numeroB = 4;
+const NUMEROC = 5;
+
+let resultadoSuma = numeroA + numeroB;
+let resultadoProducto = numeroA * NUMEROC;
+
+console.log(resultadoSuma);
+console.log(resultadoProducto);
+
+
+
+//Como interactuar con el usuario
+
+//PROMPT 
+
+let nombreIngresado = prompt("Ingrese su nombre");
+
+//Console.log
+
+console.log(nombreIngresado);
+
+//alert 
+
+alert("Bienvenido al sitio");
+
+//Ejemplo de un script
+
+let entrada = prompt("Ingresar una letra");
+
+let salida = entrada + " " + "ingresada"; 
+
+alert(salida);
