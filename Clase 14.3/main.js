6136c9b7e3418b6996e3a9ddcfa13f631a8037aa
Cami <cami.pletscher@gmail.com>
2022-03-15 10:07:49 -0300

    Clase 14.3
    while, do while, switch

--- Clase 14.3/main.js
+++ Clase 14.3/main.js
@@ -1,116 +1,97 @@
-//Estructura IF
+// // ciclo for 
 
-if (true) {
-    console.log("Vas a ver este mensaje");
-}
+//     // variable ; hasta que 1 sea menor que 1 ; se suma 1 
+// for (let i = 0; i < 10; i++) {
+//     alert(i);
+// }
+// // entra al for hasta que i sea 9 y se va sumando de uno empezando de cero
 
-if (false) {
-    console.log("No se muestra");
-}
+// // let i = 0 // Desde -- se ejecuta una vez al entrar al bucle 
+// // i < 10 // condicion se comprueba antes de cada interacion del bucle, si es false el ciclo se detiene
+// // i ++ // Actualizacion -- se ejecuta desp del body
+// // alert (i) // se ejecuta hasta que la condicion sea falsa
 
+// for (let i = 10; i > 0; i--) {
+//     alert(i);
+// }
 
-// Operador de igualdad debil (loose equality)  ==
-//Coerción de tipos - cuando comparamos number y string js los convierte en un solo tipo 
+// //Tabla de multiplicar 
 
-if (2 == "2") {
-    console.log("Esto es correcto");
-}
+// let ingresarNumero = parseInt(prompt("Ingresar numero"));
 
-// Operador de igualdad estricta (strict equality)  ===
-// Compara tambien tipo de datos
+// for (let i = 1; i <= 10; i++) {
+//     let resultado = ingresarNumero * i ; 
+//     alert(ingresarNumero + " X " + i + "=" + resultado);
+// }
 
-if (2 === "2") {
-    console.log("Esto es correcto");
-}
+// //Dar turnos del 1 al 20 
 
+// for (let i = 1; i <= 20; i++) {
+//     let ingresarNombre = prompt("Ingresar su nombre: ")
+//    alert("Turno numero: " + i + " Nombre: " + ingresarNombre);
+// }
 
-let numero = 2 
+// //Sentencia break detiene un ciclo 
 
-if (numero == 2) {
-    console.log("Es verdadero");
-}
+// for (let i=1; i<=10; i++) {
+//     if (i == 5) {
+//         break
+//     }
+//     alert(i);
+// }
 
-//Estructura if else 
+// //Sentencia continue saltea un valor 
 
-let unColor = "Rojo"
+// for (let i=1; i<=10; i++) {
+//     if (i == 5) {
+//         continue
+//     }
+//     alert(i);
+// }
 
-if (unColor == "Rojo") {
-    console.log("El color es rojo");
-} else {
-    console.log("El color no es rojo");
-}
+// While se repite la instruccion mientras la condicion sea verdadera, cuando sea falso se corta automaticamente
+// El if se ejecuta una sola vez por eso es distinto de while  
 
+// let entrada = prompt("Ingrese un dato"); 
 
-let stock = 0; 
+// while (entrada != "ESC") {
+//     alert("El usuario ingreso: " + entrada);
 
-if (stock == 0) {
-    alert("Necesitamos nuevo stock de este producto");
-}
+//     entrada = prompt("Ingrese otro dato");
+// }
 
-//Validacion if else
+//DO primero se ejecuta una vez y luego evalua el while, con esto nos aseguramos que el ciclo se ejecute al menos una vez
 
+// let repetir = false
 
-let nombreDeUsuario = prompt("Ingrese su nombre de usuario");
-
-if (nombreDeUsuario == "") {
-    alert("No ha ingresado ningun nombre");
-} else {
-    alert("Nombre de usuario ingresado: " + nombreDeUsuario);
-}
-
-//ejemplo de if else if 
-
-let precio = 50; 
-
-if (precio < 20) {
-    alert("El precio es menor a 20"); 
-} else if (precio < 50) {
-    alert("Precio es menor a 50"); 
-} else if (precio < 100) {
-    alert ("precio es menor a 100"); 
-} else {
-    alert("El precio es mayor a 100");
-}
+// do {
+//     console.log("Solo una vez");
+// } while (repetir);
 
 
-// true or false 
+//Este ciclo se ejecuta la primera vez seguro y desp corta cuando el usuario ingresa algo diferente a numero lo que no se pueda parsear
 
-let numero1 = 10;
-let esMayorQue5 = (numero1 > 5); 
+// let numero = 0; 
 
-if (esMayorQue5) {
-    alert( numero1 + " es mayor que 5");
-} else {
-    alert("El" + numero1 + "no es mayor que 5");
-}
+// do {
+//     numero = prompt("Ingrese un numero ");
+//     console.log(numero);
+// } while (parseInt(numero));
 
+let entrada1 = prompt("Ingresar un nombre").toUpperCase();
 
-//condiciones con &&
+while (entrada1 != "ESC") {
+    switch (entrada1) {
+        case "ANA":
+            alert("Hola Ana")
+            break;
+        case "JUAN":
+            alert("Hola Juan")
+            break;
+        default:
+            alert("Quien sos?")
+            break;
+    }
 
-let nombreIngresado = prompt("Ingresar nombre"); 
-let apellidoIngresado = prompt("Ingresar apellido");
-
-if ( (nombreIngresado != "") && (apellidoIngresado != "")) {
-    alert("Nombre: " + nombreIngresado + "Apellido: " + apellidoIngresado);
-} else {
-    alert("Error: Falta uno de los datos"); 
-}
-
-//condiciones con || or
-
-let nombreIngresado1 = prompt("Ingresar nombre"); 
-
-if ( (nombreIngresado1 == "ANA") || (nombreIngresado1 == "ana")) {
-    alert("El nombre ingresado es Ana");
-} else {
-    alert("El nombre ingresado no es Ana"); 
-}
-
-let numero2 = parseInt(prompt("Ingrese un numero para comparar", 0)); //el 0 es de ayuda al usuario 
-let numero3 = parseInt(prompt("Ingrese otro numero", 0));
-
-if((numero2 >= 10) && (numero3 <= 50)) {
-    alert("Los numero ingresados estan entre 10 y 50 y son: " + numero2 + "y" + numero3)
-} else {
-    alert("Los numero ingresados NO estan entre 10 y 50 y son: " + numero2 + "y" + numero3)
+    entrada1 = prompt("Ingrese un nombre").toUpperCase();
 }
