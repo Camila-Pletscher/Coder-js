3d7730e328f3e4d50a765ca36e8f0ece21ea1721
Cami <camipletscher@gmail.com>
2022-04-04 21:50:33 -0300

    Clase 4.4
    DOM

---
+++ Clase 4.4/main.js
@@ -0,0 +1,45 @@
+// //Capturar por id
+
+// let div = document.getElementById("app"); 
+// let texto = document.getElementById("parrafo1")
+
+// console.log(div.innerHTML);
+// console.log(texto.innerHTML);
+
+// //Capturar por clases
+
+// let paises = document.getElementsByClassName("paises");
+
+// console.log(paises[0].innerHTML);
+
+// let contenedores = document.getElementsByTagName("div");
+
+// console.log(contenedores[0].innerHTML);
+
+// //INNER TEXT --> le estoy asignando un nuevo valor
+
+// let titulo = document.getElementById("parrafo1");
+
+
+// // titulo.innerText = "Hola coder"
+
+// const nombreUsuario = prompt("Bienvenido, ingrese su nombre");
+
+// titulo.innerText = "Hola" + nombreUsuario;
+
+// console.log(titulo.innerText);
+
+// //Asi agrego etiquetas html a un elemento por clase 
+
+// let container = document.getElementById("container");
+
+// container.innerHTML = "<h2>Hola Mundo</h2><p>Parrafo</p>"
+
+// //Agrego la clase container row al elemento container
+
+// let container = document.getElementById("container");
+
+// container.innerHTML = "<h2>Hola Mundo</h2>"
+
+// container.className = "container row"
+
