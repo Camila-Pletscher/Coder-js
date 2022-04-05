8c50c8ae57cece779d9548a59f4c31f743bb9bc2
Cami <camipletscher@gmail.com>
2022-04-05 20:45:33 -0300

    clase 4.4

--- Clase 4.4/main.js
+++ Clase 4.4/main.js
@@ -43,3 +43,51 @@
 
 // container.className = "container row"
 
+// //Creo un elemento <p> y lo agrego con append
+
+// let parrafo = document.createElement("p");
+
+// parrafo.innerHTML = "<h2>Hola Coder</h2>";
+
+// document.body.append(parrafo);
+
+// //Elimino el elemento parrafo 1 
+
+// let parrafo = document.getElementById("parrafo1");
+
+// parrafo.remove();
+
+// //Asigno valor a un input
+
+// document.getElementById("nombre").value = "Homero";
+
+// //Recorro un array que voy llenando
+
+// let padre = document.getElementById("personas");
+
+// let personas = ["Homero", "Marge", "Bart", "Lisa"];
+
+// for (const persona of personas) {
+//     let li = document.createElement("li");
+
+//     li.innerHTML = persona
+
+//     padre.appendChild(li);
+// }
+
+
+
+
+//PLANTILLAS LITERALES
+
+let producto = {id: 1, nombre: "arroz", precio: 125};
+
+let concatenado = "id: " + producto.id + "producto: " + producto.nombre
+
+let plantillas = `ID: ${producto.id} - Producto: ${producto.nombre}`
+
+
+
+
+
+
